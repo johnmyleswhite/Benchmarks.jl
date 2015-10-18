864ca60aed2f30c52a14afeb636a07d69bfdf155
Yichao Yu <yyc1992@gmail.com>
2015-10-17 22:41:33 -0400

    Fix 0.4 deprecated bindings

--- test/01_clock_resolution.jl
+++ test/01_clock_resolution.jl
@@ -1,10 +1,11 @@
 module TestClockResolution
     import Benchmarks
     using Base.Test
+    using Compat
 
     res = Benchmarks.estimate_clock_resolution(1)
 
-    @test isa(res, Uint)
+    @test isa(res, UInt)
     @test 1 <= res <= 10_000
 
     res_2 = Benchmarks.estimate_clock_resolution()
